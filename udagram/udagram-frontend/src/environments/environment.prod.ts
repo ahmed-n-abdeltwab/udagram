8799eeddb670e2e006506a25eaa72a90da743c1c
ahmednss <ahmed.n.abdeltwab@gmail.com>
2022-11-28 16:30:10 +0200

    frontend : add the api url

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://udagrameb1-env.eba-9geg9sdz.us-east-1.elasticbeanstalk.com/api/v0",
+  apiHost: "http://udagrameb1-env-1.eba-23a24w9h.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udagrameb1-env.eba-9geg9sdz.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'http://udagrameb1-env-1.eba-23a24w9h.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
