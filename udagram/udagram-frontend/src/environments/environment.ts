7d7e25e1a704d67ef6e585d405d94a8ae45ff41f
ahmednss <ahmed.n.abdeltwab@gmail.com>
2022-11-28 14:16:11 +0200

    frontend : set the api url

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udagrameb1-env.eba-bkzqcq35.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'http://udagrameb1-env.eba-9geg9sdz.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
