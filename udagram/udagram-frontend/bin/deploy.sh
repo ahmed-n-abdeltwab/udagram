5c6063428230afb3d37aa53ebb7b08333f346eed
Ahmed Nasser <ahmed.n.abdeltwab@gmail.com>
2022-11-29 15:46:11 +0200

    frontend : clean deploy scripts

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,3 +1,2 @@
-ls
 aws s3 cp --recursive --acl public-read ./www s3://$AWS_BUCKET/
 aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://$AWS_BUCKET/
