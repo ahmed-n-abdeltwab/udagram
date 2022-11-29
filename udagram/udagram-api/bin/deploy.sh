e142ffe79877e43f26970884c23510a953999507
Ahmed Nasser <ahmed.n.abdeltwab@gmail.com>
2022-11-29 15:46:42 +0200

    backend : fix deploy scripts

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,15 +1,3 @@
-cd www
-printenv > .env
 eb init $EB_APP -r $AWS_DEFAULT_REGION -p "Node.js 14 running on 64bit Amazon Linux 2"
-eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME
-eb setenv POSTGRES_PASSWORD=$POSTGRES_PASSWORD
-eb setenv POSTGRES_HOST=$POSTGRES_HOST
-eb setenv POSTGRES_DB=$POSTGRES_DB
-eb setenv AWS_BUCKET=$AWS_BUCKET
-eb setenv AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION
-eb setenv AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
-eb setenv AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
-eb setenv JWT_SECRET=$JWT_SECRET
-eb setenv URL=$URL
+eb use $EB_ENV
 eb deploy $EB_ENV
-cd ..
